1c7515726b23d9cfeebea7e2b63586d6aad450f0
elopez592 <elopez592@gmail.com>
2026-09-26 00:05:34 -0400

    Add product image data module

---
+++ app/static/product-image-data.js
@@ -0,0 +1,9 @@
+const aluminumChunks=[];
+const hduChunks=[];
+
+/* IMAGE_CHUNKS */
+
+export const productImageData={
+  'aluminum-composite-signs':'data:image/webp;base64,'+aluminumChunks.join(''),
+  'high-density-board-signs':'data:image/webp;base64,'+hduChunks.join('')
+};
